dcea5dad2f76a02c0f82a2fc99655a90525edb3f
Robodox <trinhdanielle45@gmail.com>
2020-03-09 15:57:30 -0700

    added two climb commands

---
+++ src/main/cpp/commands/command_ClimbArmByPower.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "commands/command_ClimbArmByPower.h"
+
+command_ClimbArmByPower::command_ClimbArmByPower(subsystem_Climb *subsystem_Climb, double power)
+: m_subsystem_Climb{subsystem_Climb}, m_power{power} {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(subsystem_Climb);
+}
+
+// Called when the command is initially scheduled.
+void command_ClimbArmByPower::Initialize() {
+  m_subsystem_Climb->SetClimbArmMotorPower(m_power);
+}
+
+// Called repeatedly when this Command is scheduled to run
+void command_ClimbArmByPower::Execute() {}
+
+// Called once the command ends or is interrupted.
+void command_ClimbArmByPower::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool command_ClimbArmByPower::IsFinished() { return true; }
